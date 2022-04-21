4abc6d0d3136ba058f1b4ecf95b993e7e23670dc
Shubham Indalkar <djsi55555@gmail.com>
2022-04-21 09:40:04 +0530

    Update main.py
    added support for new api

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # DONT_REMOVE_THIS
 #  TheDarkW3b (c)
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import ParseMode
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import ParseMode, Update
 import logging
 import requests
 import json
@@ -17,11 +17,11 @@ logger = logging.getLogger(__name__)
 
 TOKEN = "YOUR_TOKEN_HERE"
 
-def download(bot, update):
+def download(update: Update, context: CallbackContext):
     message = update.effective_message
     instagram_post = message.text
     if instagram_post=="/start":
-        bot.send_chat_action(chat_id=update.message.chat_id, action="typing")
+        context.bot.send_chat_action(chat_id=update.message.chat_id, action="typing")
         update.message.reply_text("❤️ Thanks For Using Me Just Send Me The Link In Below Format  \n🔥 Format :- https://www.instagram.com/p/B4zvXCIlNTw/ \nVideos Must Be Less Then 20MB, For Now It Cannot Support Long IGTV Videos \n\n<b>Support Group :-</b> @Technology_Arena \n<b>🌀 Source</b> \nhttps://github.com/TheDarkW3b/instagram", parse_mode=ParseMode.HTML, disable_web_page_preview=True)
     else:
         pass
@@ -34,31 +34,31 @@ def download(bot, update):
             visit = requests.get(url).json()
             checking_video = visit['graphql']['shortcode_media']['is_video']
         except:
-            bot.sendMessage(chat_id=update.message.chat_id, text="Send Me Only Public Instagram Posts ⚡️")
+            context.bot.sendMessage(chat_id=update.message.chat_id, text="Send Me Only Public Instagram Posts ⚡️")
         
         if checking_video==True:
             try:
                 video_url = visit['graphql']['shortcode_media']['video_url']
-                bot.send_chat_action(chat_id=update.message.chat_id, action="upload_video")
-                bot.sendVideo(chat_id=update.message.chat_id, video=video_url)
+                context.bot.send_chat_action(chat_id=update.message.chat_id, action="upload_video")
+                context.bot.sendVideo(chat_id=update.message.chat_id, video=video_url)
             except:
                 pass
 
         elif checking_video==False:
             try:
                 post_url = visit['graphql']['shortcode_media']['display_url']
-                bot.send_chat_action(chat_id=update.message.chat_id, action="upload_photo")
-                bot.sendPhoto(chat_id=update.message.chat_id, photo=post_url)
+                context.bot.send_chat_action(chat_id=update.message.chat_id, action="upload_photo")
+                context.bot.sendPhoto(chat_id=update.message.chat_id, photo=post_url)
             except:
                 pass
         else:
-            bot.send_chat_action(chat_id=update.message.chat_id, action="typing")
-            bot.sendMessage(chat_id=update.message.chat_id, text="I Cant Send You Private Posts :-( ")
+            context.bot.send_chat_action(chat_id=update.message.chat_id, action="typing")
+            context.bot.sendMessage(chat_id=update.message.chat_id, text="I Cant Send You Private Posts :-( ")
     else:
-        bot.sendMessage(chat_id=update.message.chat_id, text="Kindly Send Me Public Instagram Video/Photo Url")
+        context.bot.sendMessage(chat_id=update.message.chat_id, text="Kindly Send Me Public Instagram Video/Photo Url")
 
 def main():
-    updater = Updater(TOKEN)
+    updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
     logger.info("Setting Up MessageHandler")
     dp.add_handler(MessageHandler(Filters.text, download))
